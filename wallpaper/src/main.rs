0edc5e08717844e4e35d38b75892db5878de1297
Blas Rabella <b.rabella@stuart.com>
2025-02-03 12:46:37 +0100

    update my configs

--- wallpaper/src/main.rs
+++ wallpaper/src/main.rs
@@ -1,15 +1,18 @@
 use noise::NoiseFn;
+use wallpaper_rs::{Desktop, DesktopEnvt};
 // #2e3440 #3b4252 #434c5e #4c566a #d8dee9 #e5e9f0 #eceff4
+//
 fn main() {
-    let scale = 0.002;
+    let scale = 0.00095;
     // let grad = colorgrad::CustomGradient::new()
     //     .html_colors(&[
-    //         "#2e3440", " #3b4252", "#434c5e", "#4c566a", "#d8dee9", "#e5e9f0", "#eceff4",
+    //         "#181825", "#1e1e2e", "#b4befe", "#89b4fa", "#74c7ec", "#89dceb",
+    //         "#94e2d5", // "#2e3440", " #3b4252", "#434c5e", "#4c566a", "#d8dee9", "#e5e9f0", "#eceff4",
     //     ])
     //     .build()
     //     .unwrap()
-    //     .sharp(7, 0.0);
-    let grad = colorgrad::viridis().sharp(16, 0.01);
+    //     .sharp(7, 0.01);
+    let grad = colorgrad::viridis().sharp(14, 0.01);
     let ns = noise::OpenSimplex::new(rand::random());
     let mut imgbuf = image::ImageBuffer::new(4048, 2048);
 
@@ -19,7 +22,10 @@ fn main() {
         *pixel = image::Rgba(rgba);
     }
 
-    imgbuf.save("/home/b.rabella/noise.png").unwrap();
+    imgbuf.save("/Users/b.rabella/noise.png").unwrap();
+    let desktop = DesktopEnvt::new().unwrap();
+    desktop.set_wallpaper("/Users/b.rabella/noise.png").unwrap();
+    println!("{:?}", desktop.get_wallpaper());
 }
 
 // Map t which is in range [a, b] to range [c, d]
