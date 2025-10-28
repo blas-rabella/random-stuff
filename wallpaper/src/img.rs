7947ce2fdebf878aabbdecf1d4987926e859343b
Blas Rabella <blas.rabella@gmail.com>
2025-10-28 12:41:34 +0100

    clippy changes

--- wallpaper/src/img.rs
+++ wallpaper/src/img.rs
@@ -4,10 +4,10 @@ use image::{ImageBuffer, Rgba};
 use crate::cli::ColorScheme;
 
 pub fn build_image(data: Vec<Color>, width: u32, height: u32) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
-    let img_buf = image::ImageBuffer::from_fn(width, height, |x, y| {
+    
+    image::ImageBuffer::from_fn(width, height, |x, y| {
         image::Rgba(data[(x + y * width) as usize].to_rgba8())
-    });
-    img_buf
+    })
 }
 
 pub fn get_color_grad(color_scheme: &ColorScheme) -> Gradient {
